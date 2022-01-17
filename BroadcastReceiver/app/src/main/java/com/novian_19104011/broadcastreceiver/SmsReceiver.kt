18ef6209cbfa5ff88917e036eb6c7b0028b2a482
Novian Dwi Romadon <noviandwiromadon74@gmail.com>
2022-01-17 14:14:20 +0700

    Pertemuan 4

---
+++ BroadcastReceiver/app/src/main/java/com/novian_19104011/broadcastreceiver/SmsReceiver.kt
@@ -0,0 +1,51 @@
+package com.novian_19104011.broadcastreceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.telephony.SmsMessage
+import android.util.Log
+
+class SmsReceiver : BroadcastReceiver() {
+
+    private val TAG = SmsReceiver::class.java.simpleName
+
+    // Berjalan dengan sendirinya ketika receiver menerima broadcast yang
+    // sesuai dengan identitas yang telah di tetapkan
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        val bundle = intent.extras
+        try {
+            if (bundle != null) {
+                val pdusObj = bundle.get("pdus") as Array<Any>
+                for (aPdusObj in pdusObj) {
+                    val currentMessage = getIncomingMessage(aPdusObj, bundle)
+                    val senderNum = currentMessage.displayOriginatingAddress.toString()
+                    val message = currentMessage.displayMessageBody
+                    Log.d(TAG, "senderNum: $senderNum; message: $message")
+                    val showSmsIntent = Intent(context, SmsReceiverActivity::class.java)
+                    showSmsIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_NO, senderNum)
+                    showSmsIntent.putExtra(SmsReceiverActivity.EXTRA_SMS_MESSAGE, message)
+                    context.startActivity(showSmsIntent)
+                }
+            }
+        }
+        catch (e: Exception) {
+            Log.d(TAG, "Exception smsReceiver$e")
+        }
+    }
+
+    // Berguna untuk menangkap pesan SMS
+    private fun getIncomingMessage(aObject: Any, bundle: Bundle): SmsMessage {
+        val currentSMS: SmsMessage
+        if (Build.VERSION.SDK_INT >= 23) {
+            val format = bundle.getString("format")
+            currentSMS = SmsMessage.createFromPdu(aObject as ByteArray, format)
+        }
+        else currentSMS = SmsMessage.createFromPdu(aObject as ByteArray)
+        return currentSMS
+    }
+}
